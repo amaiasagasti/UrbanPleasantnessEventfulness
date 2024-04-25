051b2c7f0a8ac9eb2bf93da51b8130679665aa58
Amaia <amaiasagastim@gmail.com>
2024-04-25 11:39:49 +0200

    small fix

--- code/generate_features.py
+++ code/generate_features.py
@@ -1,9 +1,4 @@
 import sys
-from Mosqito.loadFiles import load
-from SoundLights.features import extract_ARAUS_features, extract_Freesound_features
-import pandas as pd
-import os
-import json
 from SoundLights.dataset_functions import generate_features
 
 sys.path.append("..")
@@ -11,8 +6,8 @@ sys.path.append("..")
 
 # Inputs
 audios_path = "data/soundscapes_augmented/"
-ARAUScsv_path = "data/responses_SoundLights.csv"
+ARAUScsv_path = "data/csv_files/responses_SoundLights.csv"
 saving_path = "data/"
 
 # Call function
-generate_features(audios_path, ARAUScsv_path, saving_path)
+generate_features(audios_path, ARAUScsv_path, saving_path, ["ARAUS", "Freesound"])
