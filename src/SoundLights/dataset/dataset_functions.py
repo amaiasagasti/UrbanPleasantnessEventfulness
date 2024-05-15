413f85f5f08bcde9daa8e38b8a6a11b0b64dea96
Amaia <amaiasagastim@gmail.com>
2024-05-15 12:11:49 +0200

    new general generate features (not checked)

--- src/SoundLights/dataset/dataset_functions.py
+++ src/SoundLights/dataset/dataset_functions.py
@@ -600,7 +600,8 @@ def generate_features_new_audios(
             audio_l, fs = load(audio_path, wav_calib=gain, ch=0)  # L
 
             if "Freesound" or "embedding" in type:
-                # Normalisation gain to avoid a lot of clipping
+                # Normalisation gain to avoid a lot of clipping (because audio variables
+                # are in Pascal peak measure, we need "digital version")
                 norm_gain = 6.44
                 # Apply norm gain to audio
                 adapted_audio_r = audio_r / norm_gain
@@ -611,12 +612,13 @@ def generate_features_new_audios(
                 # Clipping?
                 if max_gain > 1 or min_gain < -1:
                     adapted_signal = np.clip(adapted_signal, -1, 1)
-                # Save audio
+                # Save audio provisionally
                 provisional_savingPath = saving_path + "provisional/"
                 if not os.path.exists(provisional_savingPath):
                     os.makedirs(provisional_savingPath)
                 provisional_saving_path_complete = provisional_savingPath + file
                 save_wav(adapted_signal, fs, provisional_saving_path_complete)
+                # This audio is used to generate Freesound or CLAP embedding group features
 
             ## PSYCHOACOUSTIC FEATURES EXTRACTION ########################################################
             if "ARAUS" in type:
@@ -661,11 +663,304 @@ def generate_features_new_audios(
             # Add this audio's dict to general dictionary
             output[int(files_count)] = audio_info_json
 
-            # Save info in JSON
+            # Save info in individual JSON for current audio
             if not os.path.exists(individual_json_path):
                 os.makedirs(individual_json_path)
             with open(json_name, "w") as json_file:
                 json.dump(audio_info_json, json_file, indent=4)
+
+            print("Done audio ", files_count)
+            files_count = files_count + 1
+            print(" - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+
+    # Save in json
+    if first_wav != None:
+        first_wav = first_wav.split(".")[0]
+        last_wav = last_wav.split(".")[0]
+        csv_base_name = "from_" + first_wav + "_to_" + last_wav
+        # Check if the saving directory exists, create it if it doesn't
+        json_name = saving_path + "Sounds_" + csv_base_name + ".json"
+        with open(json_name, "w") as json_file:
+            json.dump(output, json_file, indent=4)
+
+    return output
+
+
+def file_origin_info(file, participant, gain, audio_info, origin):
+    audio_info_json = {}
+
+    if origin == "new_data":
+        # Calculate mean Pleasantness and Eventfulness values
+        P, E = calculate_P_E(audio_info)
+        # Add basic info about audio to dictionary
+        audio_info_json["info"] = {
+            "file": file,
+            "fold": int(6),
+            "wav_gain": gain,
+            "Leq_R_r": float(audio_info["info.Leq_R_r"].values[0].replace(",", ".")),
+            "P_ground_truth": P,
+            "E_ground_truth": E,
+            "masker_bird": int(audio_info["info.masker_bird"].values[0]),
+            "masker_construction": int(
+                audio_info["info.masker_construction"].values[0]
+            ),
+            "masker_silence": int(audio_info["info.masker_silence"].values[0]),
+            "masker_traffic": int(audio_info["info.masker_traffic"].values[0]),
+            "masker_water": int(audio_info["info.masker_water"].values[0]),
+            "masker_wind": int(audio_info["info.masker_wind"].values[0]),
+        }
+    elif origin == "ARAUS_original":
+        audio_info_json["info"] = {
+            "file": file,
+            "participant": participant,
+            "fold": int(audio_info["fold_r"].values[0]),
+            "soundscape": audio_info["soundscape"].values[0],
+            "masker": audio_info["masker"].values[0],
+            "smr": int(audio_info["smr"].values[0]),
+            "stimulus_index": int(audio_info["stimulus_index"].values[0]),
+            "wav_gain": float(audio_info["wav_gain"].values[0]),
+            "time_taken": audio_info["time_taken"].values[0],
+            "is_attention": int(audio_info["is_attention"].values[0]),
+            "pleasant": int(audio_info["pleasant"].values[0]),
+            "eventful": int(audio_info["eventful"].values[0]),
+            "chaotic": int(audio_info["chaotic"].values[0]),
+            "vibrant": int(audio_info["vibrant"].values[0]),
+            "uneventful": int(audio_info["uneventful"].values[0]),
+            "calm": int(audio_info["calm"].values[0]),
+            "annoying": int(audio_info["annoying"].values[0]),
+            "monotonous": int(audio_info["monotonous"].values[0]),
+            "appropriate": int(audio_info["appropriate"].values[0]),
+            "P_ground_truth": audio_info["P_ground_truth"].values[0],
+            "E_ground_truth": audio_info["E_ground_truth"].values[0],
+            "Leq_R_r": audio_info["Leq_R_r"].values[0],
+            "masker_bird": int(audio_info["masker_bird"].values[0]),
+            "masker_construction": int(audio_info["masker_construction"].values[0]),
+            "masker_silence": int(audio_info["masker_silence"].values[0]),
+            "masker_traffic": int(audio_info["masker_traffic"].values[0]),
+            "masker_water": int(audio_info["masker_water"].values[0]),
+            "masker_wind": int(audio_info["masker_wind"].values[0]),
+        }
+    elif origin == "ARAUS_extended":
+        # Add basic info about audio to dictionary
+        audio_info_json["info"] = {
+            "file": file,
+            "participant": participant,
+            "fold": int(audio_info["info.fold"].values[0]),
+            "soundscape": audio_info["info.soundscape"].values[0],
+            "masker": audio_info["info.masker"].values[0],
+            "smr": int(audio_info["info.smr"].values[0]),
+            "stimulus_index": int(audio_info["info.stimulus_index"].values[0]),
+            "wav_gain": float(audio_info["info.wav_gain"].values[0]),
+            "time_taken": audio_info["info.time_taken"].values[0],
+            "is_attention": int(audio_info["info.is_attention"].values[0]),
+            "pleasant": int(audio_info["info.pleasant"].values[0]),
+            "eventful": int(audio_info["info.eventful"].values[0]),
+            "chaotic": int(audio_info["info.chaotic"].values[0]),
+            "vibrant": int(audio_info["info.vibrant"].values[0]),
+            "uneventful": int(audio_info["info.uneventful"].values[0]),
+            "calm": int(audio_info["info.calm"].values[0]),
+            "annoying": int(audio_info["info.annoying"].values[0]),
+            "monotonous": int(audio_info["info.monotonous"].values[0]),
+            "appropriate": int(audio_info["info.appropriate"].values[0]),
+            "P_ground_truth": audio_info["info.P_ground_truth"].values[0],
+            "E_ground_truth": audio_info["info.E_ground_truth"].values[0],
+            "Leq_R_r": audio_info["info.Leq_R_r"].values[0],
+            "masker_bird": int(audio_info["info.masker_bird"].values[0]),
+            "masker_construction": int(
+                audio_info["info.masker_construction"].values[0]
+            ),
+            "masker_silence": int(audio_info["info.masker_silence"].values[0]),
+            "masker_traffic": int(audio_info["info.masker_traffic"].values[0]),
+            "masker_water": int(audio_info["info.masker_water"].values[0]),
+            "masker_wind": int(audio_info["info.masker_wind"].values[0]),
+        }
+    return audio_info_json
+
+
+def generate_features_general(
+    audioFolderPath: str,
+    csvPath: str,
+    saving_path: str,
+    type: list,
+    origin: str,
+    norm_gain: float = 1,
+):
+    """
+    IN PROCESS
+    Function to generate features from any input set of audios the desired
+    acoustic and psychoacoustic features found in ARAUS dataset and the signal
+    processing features extracted with Freesound Feature Extractor.
+
+    Input:
+    audioFolderPath: relative path to the folder that contains the
+        ARAUS augmented soundscapes (.wav files)
+    csvPath: absolute path to csv file that contains gain
+    savingPath: save where output JSON is desired to be saved
+    type: type of features to generate ["ARAUS", "Freesound"]
+
+    Returns:
+        output: version/subset of ARAUS-extended JSON file containing selected features of
+            the corresponding audio files. It is directly saved in /data. It can be imported
+            as a Pandas dataframe using import_json_to_dataframe() from dataset_functions.py
+    """
+    output = {}
+    files_count = 0
+    # CHANGE READ OUTSIDE !!!!!!
+    csv_file = pd.read_csv(csvPath, delimiter=";")
+
+    # Find the first and last WAV files for json name
+    first_wav = None
+    last_wav = None
+
+    # Run only once
+    if "embedding" in type:
+        # Load the model
+        print("------- code starts -----------")
+        model = CLAP_Module(enable_fusion=True)
+        print("------- clap module -----------")
+        model.load_ckpt("data/models/630k-fusion-best.pt")
+        print("------- model loaded -----------")
+
+    # Go over each audio file
+    files = sorted(os.listdir(audioFolderPath))
+    for file in files:
+        if file.endswith(".mp3") or file.endswith(".wav"):
+            print("File ", file)
+
+            # Find the first and last WAV files for json name
+            if first_wav is None:
+                first_wav = file
+            last_wav = file
+
+            # Check if this audio had already been processed by checking if json exists
+            individual_json_path = saving_path + "individual_jsons/"
+            csv_base_name = file.split(".")[0]
+            json_name = str(individual_json_path + str(csv_base_name) + ".json")
+            if os.path.exists(json_name):
+                continue
+
+            audio_path = audioFolderPath + file
+            # Find the row in csv that the audio filename matches to get wav gain
+            if origin == "new_data":
+                audio_info = csv_file[csv_file["info.file"] == file]
+                gain = float(audio_info["info.wav_gain"].values[0].replace(",", "."))
+                participant = "mean_of_all"
+            elif origin == "ARAUS_extended":
+                file_split = file.split("_")
+                file_fold = int(file_split[1])
+                participant = "ARAUS_" + file_split[3]
+                file_stimulus = int(file_split[5].split(".")[0])
+                audio_info = csv_file[csv_file["info.fold"] == file_fold]
+                audio_info = audio_info[
+                    audio_info["info.stimulus_index"] == file_stimulus
+                ]
+                audio_info = audio_info[audio_info["info.participant"] == participant]
+                gain = audio_info["info.wav_gain"].values[0]
+            elif origin == "ARAUS_original":
+                file_split = file.split("_")
+                file_fold = int(file_split[1])
+                participant = "ARAUS_" + file_split[3]
+                file_stimulus = int(file_split[5].split(".")[0])
+                audio_info = csv_file[csv_file["fold_r"] == file_fold]
+                audio_info = audio_info[audio_info["stimulus_index"] == file_stimulus]
+                audio_info = audio_info[audio_info["participant"] == participant]
+                gain = audio_info["wav_gain"].values[0]
+
+            audio_info = file_origin_info(file, participant, gain, audio_info, origin)
+            """ # Calculate mean Pleasantness and Eventfulness values
+            P, E = calculate_P_E(audio_info)
+
+            # Add basic info about audio to dictionary
+            audio_info_json = {}
+            audio_info_json["info"] = {
+                "file": file,
+                "fold": int(6),
+                "wav_gain": gain,
+                "Leq_R_r": float(
+                    audio_info["info.Leq_R_r"].values[0].replace(",", ".")
+                ),
+                "P_ground_truth": P,
+                "E_ground_truth": E,
+                "masker_bird": int(audio_info["info.masker_bird"].values[0]),
+                "masker_construction": int(
+                    audio_info["info.masker_construction"].values[0]
+                ),
+                "masker_silence": int(audio_info["info.masker_silence"].values[0]),
+                "masker_traffic": int(audio_info["info.masker_traffic"].values[0]),
+                "masker_water": int(audio_info["info.masker_water"].values[0]),
+                "masker_wind": int(audio_info["info.masker_wind"].values[0]),
+            } """
+
+            audio_r, fs = load(audio_path, wav_calib=gain, ch=1)  # R
+            audio_l, fs = load(audio_path, wav_calib=gain, ch=0)  # L
+
+            if "Freesound" or "embedding" in type:
+                # Normalisation gain to avoid a lot of clipping (because audio variables
+                # are in Pascal peak measure, we need "digital version")
+                adapted_audio_r = audio_r / norm_gain
+                adapted_audio_l = audio_l / norm_gain
+                adapted_signal = np.column_stack((adapted_audio_l, adapted_audio_r))
+                max_gain = np.max(adapted_audio_r)
+                min_gain = np.min(adapted_audio_r)
+                # Clipping?
+                if max_gain > 1 or min_gain < -1:
+                    adapted_signal = np.clip(adapted_signal, -1, 1)
+                # Save audio provisionally
+                provisional_savingPath = saving_path + "provisional/"
+                if not os.path.exists(provisional_savingPath):
+                    os.makedirs(provisional_savingPath)
+                provisional_saving_path_complete = provisional_savingPath + file
+                save_wav(adapted_signal, fs, provisional_saving_path_complete)
+                # This audio is used to generate Freesound or CLAP embedding group features
+
+            ## PSYCHOACOUSTIC FEATURES EXTRACTION ########################################################
+            if "ARAUS" in type:
+                # Extract psychoacoustic features for signal
+                list = [
+                    "loudness",
+                    "sharpness",
+                    "LA",
+                    "LC",
+                    "frequency",
+                    "roughness",
+                    "fluctuation",
+                ]
+                audio_acoustic_features = extract_ARAUS_features(audio_r, fs, list)
+                # Add to dictionary
+                audio_info["ARAUS"] = audio_acoustic_features
+            ################################################################################################
+
+            ## NON-PSYCHOACOUSTIC FEATURES EXTRACTION ######################################################
+            if "Freesound" in type:
+                # Extract features for signal
+                audio_freesound_features = extract_Freesound_features(
+                    provisional_saving_path_complete
+                )
+                # Add to dictionary
+                audio_info["freesound"] = audio_freesound_features
+
+            ################################################################################################
+
+            ## EMBEDDING EXTRACTION ########################################################################
+            if "embedding" in type:
+                embedding = extract_CLAP_embeddings(
+                    provisional_saving_path_complete, model
+                )
+                audio_info["CLAP"] = embedding
+            ################################################################################################
+
+            # Delete provisional audio
+            if os.path.exists(provisional_savingPath):
+                delete_wav(provisional_saving_path_complete)
+
+            # Add this audio's dict to general dictionary
+            output[int(files_count)] = audio_info
+
+            # Save info in individual JSON for current audio
+            if not os.path.exists(individual_json_path):
+                os.makedirs(individual_json_path)
+            with open(json_name, "w") as json_file:
+                json.dump(audio_info, json_file, indent=4)
 
             print("Done audio ", files_count)
             files_count = files_count + 1
