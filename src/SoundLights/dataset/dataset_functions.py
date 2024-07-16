ad3e5e50f93205d2c714bf355bed0e6aadf95954
Amaia <amaiasagastim@gmail.com>
2024-07-16 10:41:42 +0200

    imports working from /Code

--- src/SoundLights/dataset/dataset_functions.py
+++ src/SoundLights/dataset/dataset_functions.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import time
 from CLAP.src.laion_clap import CLAP_Module
-from Mosqito.loadFiles import load
+from SoundLights.dataset.Mosqito.loadFiles import load
 from SoundLights.dataset.features import (
     extract_ARAUS_features,
     extract_Freesound_features,
--- src/scripts/dataset/dataset_Generate_features.py
+++ src/scripts/dataset/dataset_Generate_features.py
@@ -7,11 +7,18 @@ import pandas as pd
 # Suppress WavFileWarning
 warnings.filterwarnings("ignore", category=WavFileWarning)
 
+# Path importing
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.abspath(os.path.join(current_dir, "../../"))
+sys.path.append(src_dir)
+
+# Imports from this project
 from SoundLights.dataset.dataset_functions import (
     generate_features,
 )
 
 
+################# ARAUS extended dataset ############################
 # Inputs
 audios_path = "data/listening_test_audios/"
 csv_path = "data/main_files/answers_listening_tests.csv"
@@ -29,3 +36,5 @@ generate_features(
     6.44,
     1,
 )
+
+######################################################################
