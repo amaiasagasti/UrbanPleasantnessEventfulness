147209ea4707f2ba995bc37d44d724ad385abc6c
Amaia <amaiasagastim@gmail.com>
2024-07-16 10:53:12 +0200

    working

--- src/scripts/dataset/dataset_Generate_all_variations_fold0.py
+++ src/scripts/dataset/dataset_Generate_all_variations_fold0.py
@@ -1,21 +1,33 @@
 import pandas as pd
+import os
+import sys
+
+# Path importing
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.abspath(os.path.join(current_dir, "../../"))
+sys.path.append(src_dir)
+
+# Imports from this project
 from SoundLights.dataset.dataset_functions import (
     generate_features,
 )
 
-
+############### Code to generate fold0 variations (wav_gain variations) ###############
+# Inputs
 csvPath = "data/main_files/SoundLights_complete.csv"
-df = pd.read_csv(csvPath)
 audios_path = "data/soundscapes_augmented/ARAUS_fold0_01/"
+saving_folder_path = "data/output/"
 
-
+# Calculate
+df = pd.read_csv(csvPath)
 variations = ["x0_5", "x2", "x4", "x6", "random"]
 for variation in variations:
     print(variation)
-    saving_path = "data/output/variation_" + variation + "/"
-    print("saving path ", saving_path)
+    saving_path = saving_folder_path + "variation_" + variation + "/"
+    print("Saving path ", saving_path)
 
     if variation == "x0_5":
+        # wav_gain variation of x0.5 in linear scale = -6dB in decibels scale
         generate_features(
             audios_path,
             df,
@@ -26,6 +38,7 @@ for variation in variations:
             0.5,
         )
     elif variation == "x2":
+        # wav_gain variation of x2 in linear scale = +6dB in decibels scale
         generate_features(
             audios_path,
             df,
@@ -36,6 +49,7 @@ for variation in variations:
             2,
         )
     elif variation == "x4":
+        # wav_gain variation of x4 in linear scale = +12dB in decibels scale
         generate_features(
             audios_path,
             df,
@@ -46,6 +60,7 @@ for variation in variations:
             4,
         )
     elif variation == "x6":
+        # wav_gain variation of x6 in linear scale = +18dB in decibels scale
         generate_features(
             audios_path,
             df,
@@ -56,6 +71,7 @@ for variation in variations:
             6,
         )
     elif variation == "random":
+        # random wav_gain variation
         generate_features(
             audios_path,
             df,
@@ -65,3 +81,5 @@ for variation in variations:
             6.44,
             9999,
         )
+
+#######################################################################################
