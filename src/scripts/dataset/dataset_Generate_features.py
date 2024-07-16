2f8ae69a740e71967dab1d073d7e07d3a9645cfb
Amaia <amaiasagastim@gmail.com>
2024-07-16 12:33:13 +0200

    now yes working

--- src/scripts/dataset/dataset_Generate_features.py
+++ src/scripts/dataset/dataset_Generate_features.py
@@ -20,6 +20,28 @@ from SoundLights.dataset.dataset_functions import (
 
 ############### Code to generate ARAUS extended dataset ###############
 # Inputs
+audios_path = "data/soundscapes_augmented/ARAUS_fold0_01/"
+csv_path = "data/main_files/responses_SoundLights.csv"
+saving_path = "data/ARAUS_extended_delete/"
+
+
+csv_file = pd.read_csv(csv_path)
+# Call function
+generate_features(
+    audios_path,
+    csv_file,
+    saving_path,
+    ["ARAUS", "Freesound", "embedding"],
+    "ARAUS_original",
+    6.44,
+    1,
+)
+
+######################################################################
+
+
+# Code to generate features for new data (listenig tests audios) #####
+# Inputs
 audios_path = "data/listening_test_audios/"
 csv_path = "data/main_files/answers_listening_tests.csv"
 saving_path = "data/listening_test_data/"
